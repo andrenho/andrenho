d3f37cffe0603819ad01aacdf214890ac01d5269
andre.nho@gmail.com <andre.nho@gmail.com@a2e0a86e-a519-0410-a1aa-73a00b9cc101>
2013-05-16 20:05:06 +0000

    M    main.c

--- trunk/eletronica/aw_801_oscilloscope/code/computer/main.c
+++ trunk/eletronica/aw_801_oscilloscope/code/computer/main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "SDL.h"
 
-static SDL_Surface* video;
+#define W 800
+#define H 600
+
+static SDL_Surface* scr;
 
 static void init_video()
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	video = SDL_SetVideoMode(800, 600, 8, SDL_SWSURFACE);
+	scr = SDL_SetVideoMode(W, H, 8, SDL_SWSURFACE);
+
+	SDL_Color c[] = {
+		{ 0x00, 0x00, 0x00 },
+		{ 0x00, 0xff, 0x00 },
+	};
+	SDL_SetColors(scr, c, 0, 2);
 }
 
 
@@ -29,6 +39,22 @@ static void check_events()
 
 static void draw_board()
 {
+	// window
+	SDL_FillRect(scr, &((SDL_Rect){ 50, 50, H-100, H-100 }), 1);
+	SDL_FillRect(scr, &((SDL_Rect){ 53, 53, H-106, H-106 }), 0);
+
+	// horizontal lines
+	for(int i=50, j=0; i<H-100; i += (H-150) / 11, j++)
+	{
+		if(j == 6)
+			SDL_FillRect(scr, &((SDL_Rect){ 50, i, H-100, 2 }), 1);
+		else
+		{
+			int x;
+			for(x=50; x<H-50; x += 3)
+				*((Uint8*)(scr->pixels + i * scr->pitch + x)) = 1;
+		}
+	}
 }
 
 
@@ -46,5 +72,6 @@ int main(int argc, char** argv)
 	{
 		check_events();
 		update_osc();
+		SDL_Flip(scr);
 	}
 }
