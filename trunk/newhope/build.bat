6edc2455db635f22903a8743eaa39a8fedf3ea50
andre.nho@gmail.com <andre.nho@gmail.com@a2e0a86e-a519-0410-a1aa-73a00b9cc101>
2014-03-08 02:44:31 +0000

    .

--- trunk/newhope/build.bat
+++
@@ -1,5 +0,0 @@
-cd newhope
-zip -r newhope.love *
-cd ..
-copy /b win64\love64.exe+newhope\newhope.love NewHope.exe
-del newhope\newhope.love
--- trunk/newhope/newhope/physics/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-   Physics = my_require('physics.physics'),
-   PhysStaticObj = my_require('physics.physstaticobj'),
-}
-
--- vim: ts=3:sw=3:sts=3:expandtab
