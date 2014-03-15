22b48c1d443e7f88f715a2614b446c30d63ccdd1
andre.nho@gmail.com <andre.nho@gmail.com@a2e0a86e-a519-0410-a1aa-73a00b9cc101>
2014-03-15 02:16:08 +0000

    .

--- newhope/main.cc
+++ newhope/main.cc
@@ -1,17 +1,57 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include "render/renderengine.h"
 #include "render/scene.h"
 #include "render/camera.h"
 #include "render/triangle.h"
 
+
+float cam_x = 4,
+      cam_y = 3,
+      cam_z = 3;
+
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    (void) scancode;
+    (void) mods;
+
+    if(action == GLFW_PRESS) {
+        switch(key)
+        {
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, GL_TRUE);
+            break;
+        case GLFW_KEY_LEFT:
+            cam_x -= 0.3f; break;
+        case GLFW_KEY_RIGHT:
+            cam_x += 0.3f; break;
+        case GLFW_KEY_UP:
+            cam_y += 0.3f; break;
+        case GLFW_KEY_DOWN:
+            cam_y -= 0.3f; break;
+        case GLFW_KEY_COMMA:
+            cam_z += 0.3f; break;
+        case GLFW_KEY_PERIOD:
+            cam_z -= 0.3f; break;
+        default:
+            ;
+        }
+    }
+}
+
+
 int main()
 {
     render::RenderEngine engine("newhope");
+    engine.InstallKeyCallback(&key_callback);
 
     render::Camera camera;
     render::Scene scene(camera);
     render::Triangle object;
 
-	camera.setPosition(4, 4, 3);
 	camera.LookAt(0, 0, 0);
 
 	scene.AddObject(object);
@@ -19,6 +59,7 @@ int main()
 
 	while(engine.Active())
 	{
+        camera.setPosition(cam_x, cam_y, cam_z);
 		engine.ProcessEvents();
 		engine.Render(scene);
 	}
